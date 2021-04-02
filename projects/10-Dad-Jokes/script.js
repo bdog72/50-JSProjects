364a57997591445d2006e14f7b4159eabe763b11
Brian Bycynski <high5bri@hotmail.com>
2021-04-02 03:00:07 -0400

    Finished KeyCodes Project

---
+++ projects/10-Dad-Jokes/script.js
@@ -0,0 +1,25 @@
+//
+//
+
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click', generateJoke);
+
+generateJoke();
+
+function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  };
+
+  fetch('https://icanhazdadjoke.com', config).then((response) =>
+    response.json().then((data) => {
+      jokeEl.innerHTML = `
+      <p class='bozo'>${data.joke}</p>
+      `;
+    })
+  );
+}
