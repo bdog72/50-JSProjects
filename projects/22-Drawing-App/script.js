66a955e9fafd23553610bd2ff1d70536da2b406d
Brian Bycynski <high5bri@hotmail.com>
2021-04-09 03:15:32 -0400

    Finished Sticky Navbar

--- projects/22-Drawing-App/script.js
+++ projects/22-Drawing-App/script.js
@@ -2,12 +2,48 @@
 //
 
 const canvas = document.querySelector('#canvas');
+const increaseBtn = document.querySelector('#increase');
+
+const decreaseBtn = document.querySelector('#decrease');
+const sizeEl = document.querySelector('#size');
+
+const colorEl = document.querySelector('#color');
+const clearEl = document.querySelector('#clear');
+
 const ctx = canvas.getContext('2d');
 
-let size = 20;
+let size = 10;
 let color = 'black';
 
+let isPressed = false;
 let x, y;
+
+canvas.addEventListener('mousedown', (e) => {
+  isPressed = true;
+
+  x = e.offsetX;
+  y = e.offsetY;
+});
+
+canvas.addEventListener('mouseup', (e) => {
+  isPressed = false;
+
+  x = undefined;
+  y = undefined;
+});
+
+canvas.addEventListener('mousemove', (e) => {
+  if (isPressed) {
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
+
+    drawCircle(x2, y2);
+    drawLine(x, y, x2, y2);
+
+    x = x2;
+    y = y2;
+  }
+});
 
 function drawCircle(x, y) {
   ctx.beginPath();
@@ -21,9 +57,38 @@ function drawLine(x1, y1, x2, y2) {
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
   ctx.strokeStyle = color;
-  ctx.lineWidth = size;
+  ctx.lineWidth = size * 2;
   ctx.stroke();
 }
 
-// drawCircle(100, 100);
-// drawLine(100, 200, 100, 600);
+function updateSizeOnScreen(params) {
+  sizeEl.innerText = size;
+}
+
+increaseBtn.addEventListener('click', () => {
+  size += 5;
+
+  if (size > 50) {
+    size = 50;
+  }
+
+  updateSizeOnScreen();
+});
+
+decreaseBtn.addEventListener('click', () => {
+  size -= 5;
+
+  if (size < 5) {
+    size = 5;
+  }
+
+  updateSizeOnScreen();
+});
+
+colorEl.addEventListener('change', (e) => {
+  color = e.target.value;
+});
+
+clearEl.addEventListener('click', () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
